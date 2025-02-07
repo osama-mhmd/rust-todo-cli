10bdcbd3ff32cb6bac049d772c9c4725cd89a466
Osama Mohammed <docosmo010@gmail.com>
2025-02-07 20:10:14 +0200

    Basic Input Handling

--- main.rs
+++ main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io::{self, Write};
 
 fn main() {
   let result = fs::OpenOptions::new()
@@ -9,5 +10,27 @@ fn main() {
 
   if let Err(e) = result {
     println!("Error occured while opening file: {:#?}", e);
+  }
+
+  loop {
+    print!("> ");
+
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin()
+      .read_line(&mut input)
+      .expect("Failed to read line");
+
+    let mut parts = input.trim().split_whitespace();
+
+    let c = parts.next().unwrap_or("");
+    // let args: Vec<&str> = parts.collect();
+
+    match c {
+      "exit" => break,
+      "create" => panic!("Not implemented yet"),
+      _ => (),
+    }
   }
 }
