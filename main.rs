e3ff269f168c99d1cc8413369e8f0e1613392886
Osama Mohammed <docosmo010@gmail.com>
2025-02-07 18:36:52 +0200

    Read/Write tasks.txt

--- main.rs
+++ main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+
 fn main() {
-  println!("Hello, world!");
+  let result = fs::OpenOptions::new()
+    .read(true)
+    .write(true)
+    .create(true)
+    .open("tasks.txt");
+
+  if let Err(e) = result {
+    println!("Error occured while opening file: {:#?}", e);
+  }
 }
